d2abc4a908dd5760448a09ddf46b21e8085ebade
quantumness <paul.cuckoo@gmail.com>
2016-04-08 10:42:54 +0100

    Update multicurve.R
    v1.3  Removed first column bug, error with optimisation
    
    v1.0 - v1.2  Dev versions, many bugs

--- multicurve.R
+++ multicurve.R
@@ -1,5 +1,5 @@
 ####### Multicurve fitter for ABC curves ####### 
-# © Paul Cuckoo Annalect 2016
+# Paul Cuckoo Annalect 2016
 # v1.3
 # https://github.com/quantumness/work/blob/multi_ABC_curves/multicurve.R
 # 
@@ -34,25 +34,29 @@ fit <- function(data,par) {
 
 # Loop through revenue columns
 
-
 for(i in 2:revcols){
+
+  # Split data
   x <- unname(unlist(curvedata[1]))
   yi <- assign(paste("y", i, sep = ""), unname(unlist(curvedata[i])))
   y <- yi[!is.na(yi)]
   x <- x[c(1:length(y))]
+  
+  # Extract trial coefficients
   lm1 <- lm(log(max(y)/(y-1))~ 1+log(x))
   A <- max(y)
   B <- exp(coef(lm1)[1])
   C <- coef(lm1)[2] 
   
   dat <- data.frame(c(x), c(y))
-  
+
+  # Attempt to fit NLS model, if singular or other error, fit best alternative  
   tryCatch(coefy<-coef(nls(y ~ A/(1+B*(x)^C), start=list(A=A,B=B,C=C), control = list(maxiter = 5000))), error=function(e){
     result <- optim(par = c(max(y), exp(coef(lm1)[1]), coef(lm1)[2]), min.RSS, data = dat, method="BFGS")
     coefy <<- result$par
     type[i] <<- "OLS"
     })
-  R2[i-1] <- fit(dat,coefy)
+  R2[i-1] <- fit(dat,coefy) #pseudo R2 value
   coefmain <- cbind(coefmain, coefy)
 }
 final.data <- rbind(R2,coefmain)
